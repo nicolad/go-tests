9e2e6274e42c04368bc166d69d871f81b470b16c
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-12-11 01:28:46 +0200

    Small improvement.

--- arrays-and-slices/main.go
+++ arrays-and-slices/main.go
@@ -1,20 +1,13 @@
 package main
- 
-func Sum(a [5]int) int {
-	sum:=0
 
-	// for i:=0;i<len(a);i++{
-	// 	sum+=a[i]
-	// }
+func Sum(a []int) int {
+	sum := 0
 
-	for _,v:=range a{
-		sum+=v
+	for _, v := range a {
+		sum += v
 	}
 
 	return sum
 }
 
-func main() {
-	var a [5]int
-	Sum(a)
-}
+func main() {}
