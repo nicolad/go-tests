62c1fee1e12324a7d6dc425c8d09f12740c878a1
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-12-12 14:05:16 +0200

    Methods, function receiver.

--- arrays-and-slices/main.go
+++ arrays-and-slices/main.go
@@ -10,4 +10,26 @@ func Sum(a []int) int {
 	return sum
 }
 
+func SumAll(a ...[]int) []int {
+	var sums []int
+	for _, v := range a {
+		sums = append(sums, Sum(v))
+	}
+
+	return sums
+}
+
+func SumAllTails(a ...[]int) []int {
+	var sums []int
+	for _, v := range a {
+		if len(v) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := v[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
 func main() {}
---
+++ structs-methods-and-interfaces/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "math"
+
+type Circle struct {
+	Radius float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (c Circle) Area() float64 {
+	pi := math.Pi
+	return pi * c.Radius * c.Radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Width + rectangle.Width) * 2
+}
+
+func main() {}
